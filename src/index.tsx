9ea15b6858c572884a3dbc2b8df259e253551156
Joel Nash <joelnashjobs@yahoo.com>
2023-10-10 09:05:15 -0700

    added redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import { store } from './state/store';
+import { persistor, store } from './state/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,23 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import numParameterReducer from './numParameterSlice';
 import tokenSliceReducer from './tokenSlice';
 
+const persistConfig = {
+    key: 'data',
+    storage
+}
+const persistedReducer = persistReducer(persistConfig, combineReducers({numParameterReducer, tokenSliceReducer}))
+
+
+
 export const store = configureStore({
-    reducer: {numParameterReducer, tokenSliceReducer}
+    reducer: persistedReducer
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
